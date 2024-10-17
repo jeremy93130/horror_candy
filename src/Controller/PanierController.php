e980c92dc1c184705970fdc33fe4b406aa107783
jeremy93130 <jerem-du-75020@hotmail.fr>
2024-10-17 16:13:21 +0200

    ajout panier

---
+++ src/Controller/PanierController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\BonbonRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PanierController extends AbstractController
+{
+    #[Route('/panier', name: 'app_panier')]
+    public function index(Session $session): Response
+    {
+        $panier = $session->get('panier') ?? null;
+
+        return $this->render('panier/index.html.twig', [
+            'controller_name' => 'PanierController',
+            'panier' => $panier
+        ]);
+    }
+
+    #[Route('/addPanier', name: 'add_panier')]
+
+    public function addPanier(Request $request, Session $session, BonbonRepository $bonbonRepository): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $bonbon = $bonbonRepository->find($data['id']);
+        $panierSession = $session->get('panier', []);
+        $nombreArticles = $session->get('nb', 0);
+        $bonbonFound = false;
+
+        foreach ($panierSession as &$panier) {
+            // Accéder correctement à l'objet Bonbon stocké dans le panier
+            if ($bonbon->getId() == $panier['bonbon']->getId()) {
+                // Si le bonbon est déjà dans le panier, on incrémente la quantité
+                $panier['quantity']++; // Incrémenter la quantité
+                $bonbonFound = true; // Indiquer que le bonbon a été trouvé
+                break; // Sortir de la boucle
+            }
+        }
+
+        if (!$bonbonFound) {
+            // Ajouter un nouveau bonbon avec une quantité initiale de 1
+            $panierSession[] = [
+                'bonbon' => $bonbon, // Stocker l'objet bonbon
+                'quantity' => 1 // Initialiser la quantité à 1
+            ];
+            $nombreArticles++;
+        }
+
+        $session->set('panier', $panierSession);
+        $session->set('nb', $nombreArticles);
+
+        // Retourner une réponse JSON avec le nombre d'articles
+        return new JsonResponse(['nombreArticles' => $nombreArticles]);
+    }
+
+    #[Route('/supp', name: 'app_supp')]
+    public function removePanier(Request $request, Session $session): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $panierSession = $session->get('panier', []);
+        $nombreArticles = $session->get('nb', []);
+
+        dump($data);
+        die();
+        foreach ($panierSession as $key => &$panier) {
+            if ($panier['bonbon']->getId() == $data['id']) {
+                unset($panierSession[$key]);
+                $nombreArticles--;
+            }
+        }
+
+        $session->set('panier', $panierSession);
+        $session->set('nb', $nombreArticles);
+
+        return new JsonResponse(['nombreArticles' => $nombreArticles]);
+    }
+}
