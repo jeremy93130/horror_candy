382a46599b70fff9dc529ae9f600209d86509788
jeremy93130 <jeremy-du-75020@hotmail.fr>
2024-11-27 16:04:40 +0100

    ajout barre de recherche

--- public/assets/js/structure.js
+++ public/assets/js/structure.js
@@ -11,6 +11,7 @@ function addPanier(event, id) {
         }).then(function (result) {
             panier.textContent = result.nombreArticles;
         })
+        .catch(error => { throw (error) })
 }
 
 function supprimerArticle(event, id) {
@@ -27,10 +28,31 @@ function supprimerArticle(event, id) {
         .then(function (result) {
             let cible = document.querySelector('[data-id="' + id + '"]');
             panier.textContent = result.nombreArticles;
-            if(result.nombreArticles !== 0){
+            if (result.nombreArticles !== 0) {
                 cible.remove();
             } else {
                 window.location.reload();
             }
         })
+        .catch(error => { throw (error) })
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    var searchBar = document.querySelector('#searchBar');
+
+    searchBar.addEventListener('input', function (event) {
+        var produits = document.querySelectorAll('.searchCards');
+        produits.forEach(produit => {
+            let nom = produit.getAttribute('data-nom').toLowerCase();
+            if (!nom.includes(event.target.value)) {
+                produit.style.display = 'none';
+
+                if (produit.length <= 0) {
+                    produit.textContent = '<p> Aucun produit n\'a été trouvé </p>';
+                }
+            } else {
+                produit.style.display = 'block';
+            }
+        });
+    });
+});
